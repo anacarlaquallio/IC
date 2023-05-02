48677c8144f147df46e7db558db3c4e15362dde3
Ana Carla Quallio Rosa <anacarlarosa@alunos.utfpr.edu.br>
2023-05-01 22:31:23 -0300

    avancos

--- Euclid_Extended/main.py
+++ Euclid_Extended/main.py
@@ -1,21 +1,14 @@
 def eh_par(n):
     if (n % 2) == 0:
-        return False
-    else:
         return True
-
-def calcula_alfa (alfa_ant, resto_lm, quociente, k, l, n):
-    alfa_i = 0
-    alfa_i = (alfa_ant * (resto_lm[n]) - quociente[n])//resto_lm[n]
-    print ((alfa_ant * (resto_lm[n]))-quociente[n])
-    return alfa_i
+    else:
+        return False
 
 def mdc(a, b):
     resto = 1
     resto_l = []
     quociente = []
     alfa  = []
-    n = -2
     resto_l.append(a)
     resto_l.append(b)
     
@@ -23,45 +16,52 @@ def mdc(a, b):
         resto = a % b
         resto_l.append(resto)
         quociente.append(a//b)
-        a = b
+        a = b # assume rn na última iteração
         b = resto
-        n+=1
-    print (resto_l)
-    print ("N: ", n)
-    
-    resto_lm = metade_restos(resto_l)
-    alfa = []
+ 
+    print ("Tabela: ", resto_l)
+    print("Valores dos quocientes: ", quociente)
+    print("Valor de rn: ", a)
 
-    if (eh_par(n)):
-        i = n // 2
-        alfa.append(1)
-        linha = 0
+    resto_lm = metade_restos(resto_l, a)
+    print("Após a divisão por rn: ", resto_lm)
+
+    alfa = []
+    q = []
+    posicao = len(resto_lm) - 2
+    z = posicao - 2 #-2 porque desconsidera-se os dois primeiros valores, que são de a e b
+    
+    if (eh_par(z)):
+        i = z// 2
+        alfa.append(1) # alfa 0 = 1
+        linha = 1
         k = 2
-        l = 0    
+        l = 0 
         while (linha <= i):
-            q = calcula_alfa(alfa[linha], resto_lm, quociente, k, l, n)
+            q = (alfa[linha-1] * resto_lm[posicao-k] - quociente[z - l])//(resto_lm[posicao-l])
             alfa.append(q)
             k+=2
             l+=2
             linha +=1
+        print(alfa)
     else:
-        i = (n-1)// 2
+        i = (z-1)// 2
         alfa.append(1)
-        linha = 0
+        linha = 1
         k = 3
-        l = 1   
+        l = 1
         while (linha <= i):
-            q = calcula_alfa(alfa[linha], resto_lm, quociente, k, l, n)
+            q = (alfa[linha-1] * resto_lm[posicao-k] - quociente[posicao-l])//resto_lm[posicao-l]
             alfa.append(q)
             k+=2
             l+=2
             linha +=1
+        print(alfa) 
     return a
 
-def metade_restos (resto_l):
-    resto_lm = [int(linha) // 2 for linha in resto_l]
+def metade_restos (resto_l, rn):
+    resto_lm = [int(linha) // rn for linha in resto_l]
     return resto_lm
     
-
 mm = mdc(796, 518)
 print ("MDC :", mm)
