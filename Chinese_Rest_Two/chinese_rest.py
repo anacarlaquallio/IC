ec2e77ce3c5667826af85013b0895083277ff76d
Ana Carla Quallio Rosa <92097155+anacarlaquallio@users.noreply.github.com>
2023-07-10 12:17:30 -0300

    organizando

---
+++ Chinese_Rest_Two/chinese_rest.py
@@ -0,0 +1,28 @@
+import inverso as iv
+
+def inverso (b: int, d: int):
+    mdc, x, y = iv.mdc_extendido(b, d)
+    
+    if (mdc != 1):
+        b = b // mdc
+        d = d // mdc
+
+    if (x < 0): x = d + x        
+    return x, b, d, mdc
+        
+def calcula_equacao (a:int, b:int, c:int, d:int):
+    i, b, d, mdc = inverso(b, d)
+    x = (i * b * (c - a) + a)
+    gama = b * d * mdc
+    if (x > gama): x = x % gama
+    return x, gama
+
+print ("Este algoritmo resolve um sistema modular da forma:")
+print ("x ≡ a (mod b)")
+print ("x ≡ c (mod d)")
+a = int(input("Digite um valor para a: "))
+b = int(input("Digite um valor para b: "))
+c = int(input("Digite um valor para c: "))
+d = int(input("Digite um valor para d: "))
+x, gama = calcula_equacao(a, b, c, d)
+print("Possível solução que satisfaz ambas as equações: x ≡", x, "mod (",gama, ")")
